4371fbfb2afeb63bd85e6a4ef3c055ffd37d1bad
Nekorra <abhisrin@gmail.com>
2023-02-25 18:50:00 -0800

    recent changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,18 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'FOMO',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.None,
+    },
+  },
+  
 };
+
 
 export default config;
